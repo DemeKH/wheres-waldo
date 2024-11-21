f5c96279522fa1041e21a28af818e13d9c03662b
DemeKH <Demetre.khvedelidze@gmail.com>
2024-11-21 14:38:38 +0400

    Connect to Database

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
 const app = express();
@@ -8,9 +9,18 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 
-app.get("/get-data", (req, res) => {
-  res.status(200).json({ status: "success", data: "empty" });
-});
+const DB = process.env.DB;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true, // Helps with connection stability
+  })
+  .then(() => console.log("DB connection successful!"))
+  .catch((error) => {
+    console.error("DB connection error:", error.message);
+    process.exit(1);
+  });
 
 app.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`);
